e47772b4831466ca015097a76057d3283a00c581
luisEscobar1 <grifo279@gmail.com>
2019-08-08 15:47:33 -0500

    I base de datos+ clase dao aspirante

---
+++ Documents/NetBeansProjects/EventoIntergalactico/src/DAO/DAO_aspirante.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import VO.Administrador;
+import java.net.URISyntaxException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author LUIS
+ */
